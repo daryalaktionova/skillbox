fbd1bd9640566f817dfacc0e3b0f4c0cfb794726
daryalaktionova <segezmundovna@gmail.com>
2018-09-06 12:20:19 +0300

    Add couter

--- app/js/module.js
+++ app/js/module.js
@@ -152,10 +152,21 @@ let _ajaxInquiry = (function(e){
     send.addEventListener('click', _ajaxInquiry)
     
     
-
-
-
-
+    /*Счетчик */
+timeend= new Date();
+ timeend= new Date(2019, 12-1, 25< 18-1, 58);
+// Для задания даты с точностью до времени укажите дату в формате:
+// timeend= new Date(ГОД, МЕСЯЦ-1, ДЕНЬ, ЧАСЫ-1, МИНУТЫ);
+function time() {
+    today = new Date();
+    today = Math.floor((timeend-today)/1000);
+    tsec=today%60; today=Math.floor(today/60); if(tsec<10)tsec='0'+tsec;
+    tmin=today%60; today=Math.floor(today/60); if(tmin<10)tmin='0'+tmin;
+    thour=today%24; today=Math.floor(today/24);
+    timestr=thour+ ":" + tmin+":"+ "<span class='text--blue'>" + tsec + "</span>";
+    document.getElementById('time').innerHTML=timestr;
+    window.setTimeout("time()",1000);
+}
 
 
 
